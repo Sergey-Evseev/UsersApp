bc99ff8f1cccb44a0530ee98c353ff092a84e6ce
Sergey Evseev <sevseev@gmail.com>
2023-01-01 12:16:46 +0300

    input checks in button handler

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,49 @@ namespace UsersApp
 
         private void Button_Reg_Click(object sender, RoutedEventArgs e)
         {
-            string login;
+            //записываем в строковые переменные переданные значения полей текстбоксов имя и мейл и пассвордбоксов
+            string login = textBoxLogin.Text.Trim();//Trim удаляет пробелы в начале и конце строки
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox_2.Password.Trim();
+            string email = textBoxEmail.Text.ToLower(); /*приведение мейла к нижнему регистру*/
+            //проверки 
+            if (login.Length < 5) //если введенный логин меньше 5 символов (или не введен вовсе)
+            {
+                textBoxLogin.ToolTip = "Login is too short!"; //вывести соответствующее сообщение 
+                textBoxLogin.Background = Brushes.DarkRed; //через класс Brushes меняем цвет фона
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Password is too short!"; //вывести соответствующее сообщение 
+                passBox.Background = Brushes.DarkRed; //через класс Brushes меняем цвет фона
+            }
+            else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Entered passwords do not match!";
+                passBox_2.Background = Brushes.DarkRed;
+            }
+            //проверка email на длину, наличие знака собаки и точки
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "E-mail is incorrect!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else //если ошибок нет, то устанавливаем все подсказки пустыми, а фон полей прозрачным
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Everything's OK");
+            }
         }
     }
 }
