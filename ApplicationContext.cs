ec5d79f4dc8f47e3f98d9db66826f2247fb394d7
Sergey Evseev <sevseev@gmail.com>
2023-01-03 20:32:22 +0300

    Classes Users and ApplicationContext

---
+++ ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace UsersApp
+{
+    class ApplicationContext : DbContext
+    {
+        //класс DbSet хранит в виде списка элементы переданные из таблицы
+        //здесь мы создаем коллекцию элементов на основе данного класса
+        //в скобках указываем на основе какого класса будут храниться элементы
+        //название списка нужно указать как таблицу базы - Users
+        public DbSet<User> Users { get; set; }
+
+        //в конструктор ничего не принимаем, но передаем название подключения из App.config
+        //которое указано в поле name в строке с базой данных - DefaultConnection
+        public ApplicationContext() : base("DefaultConnection")
+        { 
+        
+        }
+
+        
+    }
+}
---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp
+{
+    class User
+    {
+        private int id { get; set; }
+        private string login, pass, email;
+
+        public User() { }
+
+        public User(string login, string email, string pass)
+        {
+            this.login = login;
+            this.email = email;
+            this.pass = pass;           
+        }
+    }
+}
